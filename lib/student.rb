4dde9bd28464689bb15bfa27ae0bafdd8f410439
Lauren Anderson <laurenaanderson95@gmail.com>
2020-09-21 21:28:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,8 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(id: nil, name:, grade:)
-    @id = id
-    @name = name
-    @grade = grade
+  def initialize
+    
   end
 
   def self.create_table
